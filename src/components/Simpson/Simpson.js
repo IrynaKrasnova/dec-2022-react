4c3539b582b1a7a75de0609ae30e337e57caa149
IrynaKrasnova <irene.ksn@gmail.com>
2023-04-12 13:54:35 +0300

    hw3

--- src/components/Simpson/Simpson.js
+++ src/components/Simpson/Simpson.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Simpson = () => {
+const Simpson = ({simpson}) => {
+
+    const {id,name,surname,age,info,photo} = simpson
+
+
     return (
         <div>
-            Simpson
+            <div>id:{id}</div>
+            <div>name:{name}</div>
+            <div>surname:{surname}</div>
+            <div>age:{age}</div>
+            <p>{info}</p>
+            <img src={photo} alt={name}/>
         </div>
     );
 };
